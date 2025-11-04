19655794a74177c21c8e8d40460b0c6bdbb0d6d1
Daniela Méndez <daniela.mdz.hdz@gmail.com>
2025-11-04 16:26:07 -0600

    Inicio (Docker no funciona)

---
+++ registro.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Registro</title>
+</head>
+<body>
+    <h1>Regristro del libro</h1>
+    <div>
+        <label for="titulo">Titulo</label>
+        <input type="text" name="titulo">
+        <label for="autor">Autor</label>
+        <input type="text" name="autor">
+        <label for="fecha">Fecha</label>
+        <input type="date" name="fecha">
+        <label for="portada">Portada</label>
+        <input type="image" src="" alt="">
+    </div>
+</body>
+</html>
